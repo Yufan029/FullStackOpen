b3d6780947449de4f7e1beb03ed200d895e915c1
Yufan Zhang <zhang.yufan029@gmail.com>
2025-09-09 13:44:38 +1000

    frontend notes in part2 build into dist, then copy to backend folder in part3

--- Part3/backend/index.js
+++ Part3/backend/index.js
@@ -23,6 +23,7 @@ let notes = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
