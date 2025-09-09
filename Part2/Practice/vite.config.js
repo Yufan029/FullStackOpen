7a1c75e399755273b111a2b242788b69353e3f7c
Yufan Zhang <zhang.yufan029@gmail.com>
2025-09-09 14:43:18 +1000

    Config proxy for phonebook client(part2) to connect backend in part3, attempt 2

--- Part2/Practice/vite.config.js
+++ Part2/Practice/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        traget: 'http://localhost:3001',
+        target: 'http://localhost:3001',
         changeOrigin: true,
       },
     }
