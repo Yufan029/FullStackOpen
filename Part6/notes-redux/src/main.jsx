875273927d66298230a3a3f80bf538fbab98415d
Yufan Zhang <zhang.yufan029@gmail.com>
2025-09-16 12:55:00 +1000

    Part6 chapter 1

--- Part6/notes-redux/src/main.jsx
+++ Part6/notes-redux/src/main.jsx
@@ -1,41 +1,13 @@
 import ReactDOM from 'react-dom/client'
 import { createStore } from 'redux'
 import noteReducer from './reducers/noteReducer'
+import App from './App'
+import { Provider } from 'react-redux'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-function App() {
-  return (
-    <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-export default App
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
--- Part6/notes-redux/src/reducers/noteReducer.js
+++ Part6/notes-redux/src/reducers/noteReducer.js
@@ -2,7 +2,7 @@ const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
       return [...state, action.payload]
-    case 'TOGGLE_IMPORTANT': {
+    case 'TOGGLE_IMPORTANCE': {
       const id = action.payload.id
       const noteToChange = state.find(note => note.id === id)
       const changedNote = {
@@ -14,6 +14,28 @@ const noteReducer = (state = [], action) => {
     }
     default:
       return state
+  }
+}
+
+const generateId = () => {
+  Number((Math.random() * 1000000).toFixed(0))
+}
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id }
   }
 }
 
