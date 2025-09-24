51f344fdcdfca7f6a2a44f68e0e716d1faa33272
Yufan Zhang <zhang.yufan029@gmail.com>
2025-09-24 22:07:20 +1000

    Part7 chapter4 webpack

---
+++ Part7/webpack/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
